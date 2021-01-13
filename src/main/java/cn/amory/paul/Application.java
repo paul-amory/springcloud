3b5edc19a9db228f9a744d88f37ee457b79fe48f
lijianming <lijianming@rivamed.cn>
2021-01-13 17:36:00 +0800

    初始化项目

---
+++ src/main/java/cn/amory/paul/Application.java
@@ -0,0 +1,18 @@
+package cn.amory.paul;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>描述：</p>
+ * <p>公司：成都瑞华康源科技有限公司</p>
+ * <p>作者：lijianming</p>
+ * <p>日期：2021-01-13 17:34</p>
+ * <p>版权：Rivamed-2020</p>
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
